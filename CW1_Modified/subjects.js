45a1b6e39fb10dc510a3e5967030ade70f4fe2c7
Rilwan Smith <40900605+rilwansmith@users.noreply.github.com>
2020-11-19 16:58:33 +0100

    Uploaded CW1 files
    I uploaded coursework 1 files for demonstration

---
+++ CW1_Modified/subjects.js
@@ -0,0 +1,92 @@
+let subjects = [
+    {
+        id: 1,
+        title: "Spanish",
+        location: "London",
+        price: 10000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 2,
+        title: "Physics",
+        location: "Malta",
+        price: 11000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 3,
+        title: "Music",
+        location: "London",
+        price: 11000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 4,
+        title: "Chemistry",
+        location: "York",
+        price: 12000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 5,
+        title: "English",
+        location: "Bristol",
+        price: 9000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 6,
+        title: "Computer Science",
+        location: "London",
+        price: 10000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 7,
+        title: "French",
+        location: "Oxford",
+        price: 10000,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 8,
+        title: "Japanese",
+        location: "Nigeria",
+        price: 10500,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 9,
+        title: "Maths",
+        location: "York",
+        price: 8500,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    },
+    {
+        id: 10,
+        title: "Korean",
+        location: "Bristol",
+        price: 9500,
+        image: "images/calc1.png",
+        spaces: 5,
+        quantity: 0
+    }
+]
